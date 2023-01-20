704342c5bcd4bc1c663400d13c8130a62aaa603e
Ashish Dhakal <dhakalashish18@gmail.com>
2023-01-20 17:58:56 +0545

    change:  useFormik with Yup

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,31 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./Create.css";
-import axios from '../axios.jsx';
+import axios from "../axios.jsx";
+import { useFormik } from "formik";
+import { Schema } from "../schemas";
+import { Error } from "../components/Error";
 
 const Create = () => {
-    const [taskName, setTaskName] = useState(null)
+  const initialValues = {
+    task: "",
+    is_completed: false,
+  };
 
-    const handleCreateTodo = async (e) => {
-        e.preventDefault();
-        try {
-            let res = await axios.post("/todo", { "task": taskName, "is_completed": false });
-            console.log(res.data);
-        } catch (error) {
-            alert(error)
-        }
+  const formik = useFormik({
+    validationSchema: Schema,
+    initialValues: initialValues,
+    onSubmit: (values, action) => {
+      handleCreateTodo(values, action);
+      action.resetForm({ values: initialValues });
+    },
+  });
+  const handleCreateTodo = (values) => {
+    try {
+      console.log(values, "fn valies");
+      axios.post("/todo", values);
+    } catch (error) {
+      alert(error);
     }
+  };
 
-    return (
-        <>
-            <div className="form_container">
-                <form className="form" onSubmit={handleCreateTodo}>
-                    <h2>Create Todo</h2>
-                    <input type="text" id="task" onChange={(e) => setTaskName(e.target.value)} />
-                    <input type="submit" id="submit" value="Create Task" />
-                </form>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="form_container">
+        <form className="form" onSubmit={formik.handleSubmit}>
+          <h2>Create Todo</h2>
+          <div className="Wrap Inputs">
+            <input
+              type="text"
+              id="task"
+              name="task"
+              value={formik.values?.task}
+              onChange={formik.handleChange}
+            />
+            <input
+              className="btn"
+              type="submit"
+              id="submit"
+              value="Create Task"
+            />
+            {formik.errors.task ? <Error Message={formik.errors.task} /> : null}
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default Create
+export default Create;
